bc5e80ef5c08136d67ccc1800918ccfa8c315d13
Gokul Ganesh I <igokulganesh@gmail.com>
2024-09-08 22:01:04 +0530

    items added

--- backend/src/main/java/com/invoiceguru/backend/item/Item.java
+++ backend/src/main/java/com/invoiceguru/backend/item/Item.java
@@ -3,6 +3,8 @@ package com.invoiceguru.backend.item;
 import java.math.BigDecimal;
 
 import com.invoiceguru.backend.company.Company;
+import com.invoiceguru.backend.tax.Tax;
+import com.invoiceguru.backend.unit.Unit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -35,10 +37,115 @@ public class Item {
     @JoinColumn(name = "company_id", referencedColumnName = "id")
     private Company company;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "unit_id", referencedColumnName = "id")
+    private Unit unit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tax_id", referencedColumnName = "id")
+    private Tax tax;
+
     // Enums
     public enum Purpose {
         Purchases,
         Sales,
         Both
     }
+
+    public Item(Long id, String name, Purpose purpose, BigDecimal rate, String hsnCode, String description,
+            Company company, Unit unit, Tax tax) {
+        this.id = id;
+        this.name = name;
+        this.purpose = purpose;
+        this.rate = rate;
+        this.hsnCode = hsnCode;
+        this.description = description;
+        this.company = company;
+        this.unit = unit;
+        this.tax = tax;
+    }
+
+    public Item() {
+    }
+
+    @Override
+    public String toString() {
+        return "Item [id=" + id + ", name=" + name + ", purpose=" + purpose.name() + ", rate=" + rate + ", hsnCode="
+                + hsnCode + ", description=" + description + ", company=" + company.getName() + ", unit="
+                + unit.getName() + ", tax=" + tax.getName() + "]";
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Purpose getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(Purpose purpose) {
+        this.purpose = purpose;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+
+    public String getHsnCode() {
+        return hsnCode;
+    }
+
+    public void setHsnCode(String hsnCode) {
+        this.hsnCode = hsnCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    public Tax getTax() {
+        return tax;
+    }
+
+    public void setTax(Tax tax) {
+        this.tax = tax;
+    }
+
 }
