4ed6ffbe7c0bc85137cdeaac1ecd5cdc48f2ef03
Gokul Ganesh I <igokulganesh@gmail.com>
2024-09-10 23:05:05 +0530

    added party service

---
+++ backend/src/main/java/com/invoiceguru/backend/services/party/PartyRepository.java
@@ -0,0 +1,11 @@
+package com.invoiceguru.backend.services.party;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PartyRepository extends JpaRepository<Party, Long> {
+    List<Party> findByCompanyId(Long companyId);
+}
